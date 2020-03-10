b219f550b4860613731b4de116f7cdabde9e8b35
Marcin Borowicz <marcinbor85@gmail.com>
2020-03-10 23:07:53 +0100

    allow additional special chars in command name

--- src/cat.c
+++ src/cat.c
@@ -209,7 +209,7 @@ static void prepare_search_command(struct cat_object *self)
 
 static int is_valid_cmd_name_char(const char ch)
 {
-        return (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || (ch == '+');
+        return (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || (ch == '+') || (ch == '#') || (ch == '$') || (ch == '@');
 }
 
 static int is_valid_dec_char(const char ch)
