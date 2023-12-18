349dc4888d22def6f4027c1368605f92dad62dcf
Pierre Lescure <pierre.lescure@trasna.io>
2023-12-18 15:53:37 +0100

    Clean demo.c

--- example/demo.c
+++ example/demo.c
@@ -31,93 +31,42 @@ SOFTWARE.
 
 #include "../src/cat.h"
 
-static int32_t speed;
-static uint16_t adr;
-static uint8_t x;
-static uint8_t y;
-static uint8_t bytes_buf[4];
-static char msg[16];
 static bool quit_flag;
 
-static int x_write(const struct cat_variable *var, size_t write_size)
-{
-        printf("x variable updated internally to: %u\n", x);
+
+struct spi_cmd_t {
+	uint8_t address;
+	uint16_t data;
+	unsigned char data_set_with_address;
+};
+
+struct spi_cmd_t spi_cmd; 
+
+
+static int spi_write(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num) {
+	if(spi_cmd.data_set_with_address == 0) {
+		printf("Set data before write\n");
+		return -1;
+	}
+	printf("WRITE SPI\n");
+	printf("Address=0x%02hx, Value=%d(0x%04hx)\n", spi_cmd.address, spi_cmd.data);
+	return 0;
+}
+
+static int spi_read(const struct cat_command *var, uint8_t *data, size_t *data_size, const size_t max_data_size) {
+        printf("READ SPI\n");
+		printf("Address=0x%02hx, Value=%d(0x%04hx)\n", spi_cmd.address, spi_cmd.data);
         return 0;
 }
 
-static int y_write(const struct cat_variable *var, size_t write_size)
-{
-        printf("y variable updated internally to: %u\n", y);
-        return 0;
+static int SpiResetIndex(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num) {
+	spi_cmd.data_set_with_address = 0;
+	return 0;
 }
 
-static int msg_write(const struct cat_variable *var, size_t write_size)
-{
-        printf("msg variable updated %lu bytes internally to: <%s>\n", write_size, msg);
-        return 0;
-}
-
-static int speed_write(const struct cat_variable *var, size_t write_size)
-{
-        printf("speed variable updated internally to: %d\n", speed);
-        return 0;
-}
-
-static int adr_write(const struct cat_variable *var, size_t write_size)
-{
-        printf("adr variable updated internally to: 0x%04X\n", adr);
-        return 0;
-}
-
-static int bytesbuf_write(const struct cat_variable *var, size_t write_size)
-{
-        int i = 0;
-
-        printf("bytes_buf variable updated %lu bytes internally to: ", write_size);
-        for (i = 0; i < sizeof(bytes_buf); i++)
-                printf("%02X", bytes_buf[i]);
-        printf("\n");
-
-        return 0;
-}
-
-static int go_write(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num)
-{
-        int i = 0;
-
-        printf("<%s>: x=%d y=%d msg=%s @ speed=%d\n",
-                cmd->name,
-                *(uint8_t*)(cmd->var[0].data),
-                *(uint8_t*)(cmd->var[1].data),
-                msg,
-                speed
-        );
-
-        printf("<bytes>: ");
-        for (i = 0; i < sizeof(bytes_buf); i++)
-                printf("%02X", bytes_buf[i]);
-        printf("\n");
-        return 0;
-}
-
-static int set_write(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num)
-{
-        printf("<%s>: SET SPEED TO = %d\n",
-                cmd->name,
-                speed
-        );
-        return 0;
-}
-
-static int set_read(const struct cat_command *cmd, uint8_t *data, size_t *data_size, const size_t max_data_size)
-{
-        return 0;
-}
-
-static int test_run(const struct cat_command *cmd)
-{
-        printf("TEST: <%s>", cmd->name);
-        return 0;
+static int SpiSetIndex(const struct cat_command *cmd, const uint8_t *data, const size_t data_size, const size_t args_num) {
+	spi_cmd.data_set_with_address = 1;
+	return 0;
 }
 
 static int quit_run(const struct cat_command *cmd)
@@ -132,73 +81,46 @@ static int print_cmd_list(const struct cat_command *cmd)
         return CAT_RETURN_STATE_PRINT_CMD_LIST_OK;
 }
 
-static struct cat_variable go_vars[] = {
-        {
-                .type = CAT_VAR_UINT_DEC,
-                .data = &x,
-                .data_size = sizeof(x),
-                .write = x_write,
-                .name = "x"
-        },
-        {
-                .type = CAT_VAR_UINT_DEC,
-                .data = &y,
-                .data_size = sizeof(y),
-                .write = y_write,
-                .name = "y"
-        },
-        {
-                .type = CAT_VAR_BUF_STRING,
-                .data = msg,
-                .data_size = sizeof(msg),
-                .write = msg_write,
-                .name = "msg"
-        }
-};
-
-static struct cat_variable set_vars[] = {
-        {
-                .type = CAT_VAR_INT_DEC,
-                .data = &speed,
-                .data_size = sizeof(speed),
-                .write = speed_write,
-                .name = "speed"
-        },
+static struct cat_variable data_spi[] = {
         {
                 .type = CAT_VAR_NUM_HEX,
-                .data = &adr,
-                .data_size = sizeof(adr),
-                .write = adr_write,
-                .name = "address"
-        },
-        {
-                .type = CAT_VAR_BUF_HEX,
-                .data = &bytes_buf,
-                .data_size = sizeof(bytes_buf),
-                .write = bytesbuf_write,
-                .name = "buffer"
+                .data = &(spi_cmd.address),
+                .data_size = sizeof(spi_cmd.address),
+                .write = SpiResetIndex,
+				//.read = spi_read,
+                .name = "address",
+                .access = CAT_VAR_ACCESS_READ_WRITE
+        }, {
+                .type = CAT_VAR_INT_DEC,
+                .data = &(spi_cmd.data),
+                .data_size = sizeof(spi_cmd.data),
+                .write = SpiSetIndex,
+				//.read = spi_read,
+                .name = "data",
+                .access = CAT_VAR_ACCESS_READ_WRITE
         }
 };
 
 static struct cat_command cmds[] = {
-        {
-                .name = "+GO",
-                .write = go_write,
-                .var = go_vars,
-                .var_num = sizeof(go_vars) / sizeof(go_vars[0]),
-                .need_all_vars = true
-        },
-        {
-                .name = "+SET",
-                .write = set_write,
-                .read = set_read,
-                .var = set_vars,
-                .var_num = sizeof(set_vars) / sizeof(set_vars[0]),
-        },
-        {
-                .name = "#TEST",
-                .run = test_run
-        },
+		{
+                .name           = "+SPI",
+                .description    = "Fill SPI buffer",
+                //.write          = spi_write,
+                //.read           = spi_read,
+                .var            = data_spi,
+                .var_num        = sizeof(data_spi) / sizeof(data_spi[0]),
+                .need_all_vars = false
+		},
+		{
+                .name           = "+SPIR",
+                .description    = "Read SPI from buffer",
+                .run           = spi_read
+		},
+		{
+                .name           = "+SPIW",
+                .description    = "Write SPI from buffer",
+                .run            = spi_write
+		},
         {
                 .name = "#HELP",
                 .run = print_cmd_list,
